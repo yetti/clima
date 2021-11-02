7af6e4cb80b17240b9b55d27c1fca921e5b4d1fb
Yetrina Battad <hello@yetti.io>
2021-11-02 22:12:42 +1100

    chore: refactors eslint and prettier config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,22 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import { ReactElement } from 'react'
 import Layout from '../components/layout'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>clima</title>
-        <meta name="description" content="How's the weather in your neighbourhood?" />
+        <meta
+          name="description"
+          content="How's the weather in your neighbourhood?"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
     </>
   )
 }
 
 Home.getLayout = function getLayout(page: ReactElement) {
-  return (
-    <Layout>
-      { page }
-    </Layout>
-  )
+  return <Layout>{page}</Layout>
 }
